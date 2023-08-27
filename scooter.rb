da9c060398164569f12f1fe8b96c47affe4c4b23
Ebanyyy <vaniegabu@gmail.com>
2023-08-27 12:41:36 +0800

    2nd

--- scooter.rb
+++ scooter.rb
@@ -1,18 +1,43 @@
 class Scooter 
 	def initialize(available, condition)
-		@available = false # unavailable
+		@no_scooter = no_scooter
+		@available = available # unavailable
 		@condition = "Good"
 	end
 
 	def available?
-		@available = true
+		if @available
+			@available = false
+			return true
+		else
+			return false
+		end
 	end
 
 	def condition
 		@condition 
 	end
+
+	# def scooters(availability, condition)
+	# 	scooter = scooter.find { |scooter| scooter.available? == scooter.available?}
+
+	# 	if scooter
+	# 		scooter.available(condition)
+	# 	else
+	# 		puts "#{scooter.available?} is available"
+	# 	end
+
+	# 	scooters << scooter
+	# end
 end
 
-scooter = Scooter.new("available", "Good")
-puts scooter.available?
-puts scooter.condition
+
+
+# scooter1 = Scooter.new("available", "good")
+# scooter2 = Scooter.new("available", "good")
+# scooter3 = Scooter.new("unvailable", "broken")
+# puts scooter1.available?
+# puts scooter1.condition
+
+
+
